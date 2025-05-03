82041bfe3a1a327534beef4b5c2e9010318962e5
Alfhi Saputra <muhalfhisaputra@gmail.com>
2025-05-03 10:39:26 +0700

    update webcam

--- app.py
+++ app.py
@@ -40,12 +40,15 @@ webrtc_streamer(
     async_transform=True,
     rtc_configuration={
         "iceServers": [
-            {"urls": ["stun:stun.l.google.com:19302"]}
+            {"urls": ["stun:stun.l.google.com:19302"]},
+            {
+                "urls": ["turn:openrelay.metered.ca:80"],
+                "username": "openrelayproject",
+                "credential": "openrelayproject"
+            }
         ]
     }
 )
-
-
 
 # === Footer ===
 st.markdown("---")
